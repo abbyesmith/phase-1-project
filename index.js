64a4907751b3f81a22fcfc96988fbfd385756b6f
Abby <abby.e.smith.85@gmail.com>
2023-02-09 16:25:19 -0700

    Started building cocktail list

--- index.js
+++ index.js
@@ -1,1 +1,85 @@
-
+{
+    "menu" : [
+        {
+            "id" : 1,
+            "name" : "Margarita",
+            "image" : "",
+            "ingredient 1" : "1.5 oz silver tequila",
+            "ingredient 2" : "1 oz orange liqueur",
+            "ingredient 3" : ".75 oz freshly-squeezed lime juice",
+            "ingredient 4" : "Optional: .75 oz simple syrup or agave nectar",
+            "ingredient 5" : "1 cup ice",
+            "ingredient 6" : "lime wedge",
+            "ingredient 7" : "coarse salt for rimming glass",
+            "ingredient 8" : "",
+            "ingredient 9" : "",
+            "ingredient 10" : "",
+            "directions" : "Add the ",
+            "source" : "https://www.gimmesomeoven.com/margarita-recipe/"
+        },
+        {
+            "id" : 2,
+            "name" : "Old Fashioned",
+            "image" :
+            "ingredient 1" :
+            "ingredient 2" :
+            "ingredient 3" :
+            "ingredient 4" :
+            "ingredient 5" :
+            "ingredient 6" :
+            "ingredient 7" :
+            "ingredient 8" :
+            "ingredient 9" :
+            "ingredient 10" :
+            "directions" :
+        },
+        {
+            "id" : 3
+            "name" : "Bee's Knees",
+            "image" :
+            "ingredient 1" :
+            "ingredient 2" :
+            "ingredient 3" :
+            "ingredient 4" :
+            "ingredient 5" :
+            "ingredient 6" :
+            "ingredient 7" :
+            "ingredient 8" :
+            "ingredient 9" :
+            "ingredient 10" :
+            "directions" :
+        },
+        {
+            "id" : 4
+            "name" : "Mojito",
+            "image" :
+            "ingredient 1" :
+            "ingredient 2" :
+            "ingredient 3" :
+            "ingredient 4" :
+            "ingredient 5" :
+            "ingredient 6" :
+            "ingredient 7" :
+            "ingredient 8" :
+            "ingredient 9" :
+            "ingredient 10" :
+            "directions" :
+        },
+        {
+            "id" : 5
+            "name" : "Mimosa",
+            "image" :
+            "ingredient 1" :
+            "ingredient 2" :
+            "ingredient 3" :
+            "ingredient 4" :
+            "ingredient 5" :
+            "ingredient 6" :
+            "ingredient 7" :
+            "ingredient 8" :
+            "ingredient 9" :
+            "ingredient 10" :
+            "directions" :
+        }
+    ]
+}
